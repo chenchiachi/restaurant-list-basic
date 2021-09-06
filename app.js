f9ae193c099ba9cab89095a4672a958ee273d647
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-09-06 21:44:26 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  res.send('This is restaurant list built with Express')
+})
+
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
