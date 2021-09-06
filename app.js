d184274d2053c94af5d6c63e998555ad66b2d46e
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-09-06 23:00:21 +0800

    Add detail page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 const app = express()
 const port = 3000
 
@@ -8,9 +9,13 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 
+app.get('/restaurants/:id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
+  res.render('detail', { restaurant: restaurant })
+})
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
